5a76576fb97c2b01165a1ae1f28cad2b6c6b930b
Michael Bøje Skjærlund <michael_b_hansen@live.dk>
2017-08-26 19:54:42 +0200

    made republish

---
+++ DISA/bin/Release/PublishOutput/wwwroot/js/AddShowTimeButtonHandler.js
@@ -0,0 +1,13 @@
+﻿$("#addShowTimeToForm").on("click", function (e) {
+    e.preventDefault();
+    // Appending to bottom of wrapper div everytime user presses the add button
+    $("#multiShowTimeInputWrapper").append('<span class="input-remove-group"><div class="input"><input type="datetime" name="showtime" class="form-control" placeholder="YYYY-MM-DD HH:MI:SS" /><input id="showtimeTheaterInput" type="number" name="theater" class="form-control theaterNumberInput" placeholder="1-10"/><img src="/images/site/Delete-128.png" /></span>');
+
+    // removing on user click red cross
+    $("#multiShowTimeInputWrapper span img").on("click", function () {
+        $(this).parent().remove();
+    });
+    
+
+
+});
